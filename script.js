3f4e94decfade7283979f7b95dbed1e9763df631
ColonelK9 <krupagabrielalfred@gmail.com>
2022-05-14 22:55:23 +0200

    edit function computerPlay to return lowercase

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-//computer randomly returns 'Rock', 'Paper' or 'Scissors'
+//computer randomly returns 'rock', 'paper' or 'scissors'
 function computerPlay() {
-    const output = ['Rock', 'Paper', 'Scissors'];
+    const output = ['rock', 'paper', 'scissors'];
     let index = Math.floor(Math.random()*output.length);
+    
     return output[index];
 } //works
 
 
+//play a single round of Rock Paper Scissors
+function playRound() {
+    
+}
